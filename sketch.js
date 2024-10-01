8cd107f51a9da1bbc500a1a9c14c08f71d3336b1
Dajohnson000 <daj22c@fsu.edu>
2024-10-01 10:28:18 -0400

    Imgs in but need help

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+
+
+let img = []
+let x;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  img = loadImage('FLowers.jpg');
+  img[1] = loadImage('GEDC0072.JPG');
+  img[2] = loadImage('Foliage.jpg');
+  img[3] = loadImage('gift for mom.jpg');
+
 }
 
+
 function draw() {
-  background(125);
+  background(0, 255, 0);
+  image(img, 0, 0);
+}
+function keyPressed(){
+(img ++)
+
 }
