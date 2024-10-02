c9c5982c46b9f8a167bae922e639825f99c76cd2
Dajohnson000 <daj22c@fsu.edu>
2024-10-02 10:46:09 -0400

    better images for test

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ let shakes = 0;
 let minimumShakes = 5;
 
 function preload() {
-  images[0] = loadImage("FLowers.jpg");
-  images[1] = loadImage("GEDC0072.JPG");
-  images[2] = loadImage("Foliage.jpg");
-  images[3] = loadImage("gift for mom.jpg");
+  images[0] = loadImage("test 1.jpg");
+  images[1] = loadImage("test 2.JPG");
+  images[2] = loadImage("test 3.png");
+  images[3] = loadImage("test 4.png");
 }
 
 function setup() {
