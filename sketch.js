65bd9c92d1b3a8e76dce1d53dc4b776be9241a28
Dajohnson000 <daj22c@fsu.edu>
2024-10-02 11:14:13 -0400

    Test git again

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ let shakes = 0;
 let minimumShakes = 5;
 
 function preload() {
-  images[0] = loadImage("test 1.jpg");
-  images[1] = loadImage("test 2.JPG");
-  images[2] = loadImage("test 3.png");
-  images[3] = loadImage("test 4.png");
+  images[0] = loadImage("assets/test 1.jpg");
+  images[1] = loadImage("assets/test 2.JPG");
+  images[2] = loadImage("assets/test 3.png");
+  images[3] = loadImage("assets/test 4.png");
 }
 
 function setup() {
