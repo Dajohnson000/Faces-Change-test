7b4bd195a97382ed8b4fa9e68caa756113089059
Dajohnson000 <daj22c@fsu.edu>
2024-10-02 10:32:45 -0400

    "shakes" works

--- sketch.js
+++ sketch.js
@@ -1,21 +1,33 @@
+let images = [];
+let i = 0;
+let shakes = 0;
+let minimumShakes = 5;
+let recentlyShaken = false;
 
-let images = []
-let i = 0
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  images[0] = loadImage('FLowers.jpg');
-  images[1] = loadImage('GEDC0072.JPG');
-  images[2] = loadImage('Foliage.jpg');
-  images[3] = loadImage('gift for mom.jpg');
-
+function preload() {
+  images[0] = loadImage("FLowers.jpg");
+  images[1] = loadImage("GEDC0072.JPG");
+  images[2] = loadImage("Foliage.jpg");
+  images[3] = loadImage("gift for mom.jpg");
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  setShakeThreshold(50);
+}
 
 function draw() {
   background(0, 255, 0);
   image(images[i], 0, 0);
 }
-function keyPressed(){
-i = (i + 1) % 4
-console.log(i);
+function keyPressed() {
+  shakes++;
+  if (shakes < minimumShakes) return;
+
+  if (shakes >= minimumShakes) {
+    i = (i + 1) % 4;
+    shakes = 0;
+  }
+
+  console.log(shakes);
 }
