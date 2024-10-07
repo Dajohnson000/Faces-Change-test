2426bbe8236b28913f3cef4e3ae4d713fa77f092
Dajohnson000 <daj22c@fsu.edu>
2024-10-07 09:43:01 -0400

    images new real

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ let shakes = 0;
 let minimumShakes = 5;
 
 function preload() {
-  images[0] = loadImage("assets/test 1.jpg");
-  images[1] = loadImage("assets/test 2.jpg");
-  images[2] = loadImage("assets/test 3.png");
-  images[3] = loadImage("assets/test 4.png");
+  images[0] = loadImage("assets/Happi.png");
+  images[1] = loadImage("assets/Sad.png");
+  images[2] = loadImage("assets/Error.png");
+  images[3] = loadImage("assets/Dizzy.png");
+  images[4] = loadImage("assets/Ded.png")
 }
 
 function setup() {
@@ -19,12 +20,13 @@ function draw() {
   background(0, 255, 0);
   image(images[i], 0, 0);
 }
+
 function deviceShaken(){
   shakes++;
   if (shakes < minimumShakes) return;
 
   if (shakes >= minimumShakes) {
-    i = (i + 1) % 4;
+    i = (i + 1) % 5;
     shakes = 0;
   }
 
